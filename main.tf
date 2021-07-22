ce21885d5878ab27379da064636eaff724d54a3f
kvchiru <87814975+kvchiru@users.noreply.github.com>
2021-07-22 16:46:19 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,27 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+resource "aws_instance" "example" {
+  ami           = "ami-066663db63b3aa675"
   instance_type = "t2.micro"
+  key_name = "rootkey"
+  security_groups = ["${aws_security_group.allow_rdp.name}"]
 
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
 }
+
+resource "aws_security_group" "allow_rdp" {
+  name        = "allow_rdp"
+  description = "Allow ssh traffic"
+
+
+  ingress {
+
+    from_port   = 3389 #  By default, the windows server listens on TCP port 3389 for RDP
+    to_port     = 3389
+    protocol =   "tcp"
+
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
